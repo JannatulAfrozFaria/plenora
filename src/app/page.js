bba17a04a92989726c58ac80606e64f19940c9e7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-21 02:47:55 +0600

    main div style cleared

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="">
        
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
