63e162ae2f6106b5599fca112e4f2f5f5184cdac
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-21 03:46:08 +0600

    container and auto margin added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100 text-primary">
+        <div className='bg-base-100 text-primary'>
+            <div className="navbar container mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
