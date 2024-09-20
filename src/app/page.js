e394b3c007680628a9d92520b9bbf0b048ab5a69
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-21 02:52:06 +0600

    HomePage imported in page.js(main)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import HomePage from "@/components/HomePage/HomePage";
 
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-       
+       <HomePage></HomePage>
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer> */}
