02af25079754c8fd11e36a712c2f89d47b130c1d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-21 03:20:42 +0600

    home height set to screen

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='h-screen'>
             <h1>Home Page -Plenora</h1>
             <p>Here is the landing page</p>
         </div>
