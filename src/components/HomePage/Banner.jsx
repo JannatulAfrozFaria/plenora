cffcceb29aea95378248de38186d8bd98ac04b8c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-22 02:35:47 +0600

    project title updated in layout

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -5,7 +5,9 @@ const Banner = () => {
         <div>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative w-full">
-                    
+                    <div>
+                        <h1>Project 1</h1>
+                    </div>
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a href="#slide4" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
