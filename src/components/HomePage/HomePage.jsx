af3d4fa44401c2b5a0711a50b2a5dda986bcb716
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-22 02:30:17 +0600

    HomePage contents removed

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div className='h-screen'>
-            <h1>Home Page -Plenora</h1>
-            <p>Here is the landing page</p>
+           
         </div>
     );
 };
