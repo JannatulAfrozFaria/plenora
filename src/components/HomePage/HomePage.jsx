aff39d0ea761b91010716f8724acaf5d83fa4683
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-22 02:31:04 +0600

    Banner imported to Home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const HomePage = () => {
     return (
         <div className='h-screen'>
-           
+           <Banner></Banner>
         </div>
     );
 };
