5fe6044d772896afd6b00d0cc238b6b2e70e8bf1
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 03:00:24 +0600

    bg set for image part

--- src/components/HomePage/Welcome.jsx
+++ src/components/HomePage/Welcome.jsx
@@ -4,7 +4,10 @@ const Welcome = () => {
     return (
         <div>
             <div className="grid grid-cols-2 gap-8">
-                <div className='absolute'>
+                <div className='absolute' style={{
+                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${banner.image})`,
+                    }}>
+                https://i.postimg.cc/Pr5rZCcz/welcome.jpg
                     <div className='relative'></div>
                     <div className='relative grid grid-cols-1 theme-bg '>
 
