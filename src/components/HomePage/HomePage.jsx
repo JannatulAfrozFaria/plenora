a22fae1b32537a5471b3e9e07b89deb9d656275d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 02:33:31 +0600

    Welcome Section created

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Welcome from './Welcome';
 
 const HomePage = () => {
     return (
         <div className=''>
            <Banner></Banner>
+           <Welcome></Welcome>
         </div>
     );
 };
