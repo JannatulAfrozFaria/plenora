e6850ede15aa153f9069583b9c564ef029c6a437
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 02:34:27 +0600

    grid created for welcome section

--- src/components/HomePage/Welcome.jsx
+++ src/components/HomePage/Welcome.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            
+            <div className="grid grid-cols-2 gap-8">
+
+            </div>
         </div>
     );
 };
