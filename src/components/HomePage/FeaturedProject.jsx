be830b9f404fbde576995ff3b6962dc1aba2d0c2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:52:43 +0600

    image added for left

--- src/components/HomePage/FeaturedProject.jsx
+++ src/components/HomePage/FeaturedProject.jsx
@@ -1,13 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
 
 const FeaturedProject = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2'>
-            <div>
-
+            <div className='grid grid-col-1'>
+                <div className='flex'>
+                <div>
+                    <Image height={900} width={800} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
+                </div>
+                <div>
+                    <Image height={900} width={800} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
+                </div>
+                </div>
+                <div></div>
             </div>
             <div>
-                
+
             </div>
         </div>
     );
