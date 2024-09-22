551b5f2aeec23e5976c382fda58419c529f3ea96
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 05:22:33 +0600

    what we do heading added

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const WhatWeDo = () => {
     return (
         <div>
-            
+            <div>
+                <h1 className='text-gray font-bold'>What we do</h1>
+            </div>
         </div>
     );
 };
