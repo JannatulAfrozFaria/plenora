bcc4ff134f134b28e9d81450360700ef6f42cc65
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:55:46 +0600

    text-section styled up

--- src/components/HomePage/FeaturedProject.jsx
+++ src/components/HomePage/FeaturedProject.jsx
@@ -6,17 +6,19 @@ const FeaturedProject = () => {
         <div className='grid grid-cols-1 md:grid-cols-2'>
             <div className='grid grid-col-1'>
                 <div className='flex'>
-                <div>
-                    <Image height={900} width={800} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
-                </div>
-                <div>
-                    <Image height={900} width={800} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
-                </div>
+                    <div>
+                        <Image height={500} width={500} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
+                    </div>
+                    <div>
+                        <Image height={500} width={500} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
+                    </div>
                 </div>
                 <div></div>
             </div>
             <div>
-
+                    <h1 className="text-theme ">Featured Project</h1>
+                    <h2 className="text-6xl text-gray">Frame House</h2>
+                    <p><span className='font-semibold'>Scope</span> Architecture/Interiors</p>
             </div>
         </div>
     );
