8df4c24a55ca4518618fcff265847c02baae0275
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:31:43 +0600

    writing section styled

--- src/components/HomePage/Process.jsx
+++ src/components/HomePage/Process.jsx
@@ -5,8 +5,8 @@ const Process = () => {
         <div className='grid grid-cols-2 gap-6 md:gap-16'>
             <div></div>
             <div>
-                <h2>Process</h2>
-                <p></p>
+                <h2 className='text-theme text-6xl'>Process</h2>
+                <p><span className='text-theme'>As a</span> full-service architectural firm, Measured is engaged in every phase of a project from concept to completion, enabling the creation of environments (building, interiors and landscape) that facilitate the desires of clients. Our goal is to design a backdrop for your lives where your experience of it is exactly what you imagined it would be at the outset.</p>
             </div>
         </div>
     );
