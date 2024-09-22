b58916ef4154caf63c92543371a8859efe89ab36
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:37:19 +0600

    style added to read

--- src/components/HomePage/Process.jsx
+++ src/components/HomePage/Process.jsx
@@ -10,7 +10,7 @@ const Process = () => {
             <div>
                 <h2 className='text-theme text-6xl'>Process</h2>
                 <p><span className='text-theme'>As a</span> full-service architectural firm, Measured is engaged in every phase of a project from concept to completion, enabling the creation of environments (building, interiors and landscape) that facilitate the desires of clients. Our goal is to design a backdrop for your lives where your experience of it is exactly what you imagined it would be at the outset.</p>
-                <a href="">Read more</a>
+                <a text-theme className='' href="">Read more</a>
             </div>
         </div>
     );
