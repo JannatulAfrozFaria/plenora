b1c17f16289341bccb65a045558e938d311906e1
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:38:18 +0600

    margin set up in p tag

--- src/components/HomePage/Process.jsx
+++ src/components/HomePage/Process.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Process = () => {
     return (
-        <div className='grid grid-cols-2 gap-6 md:gap-16'>
+        <div className='grid grid-cols-2 gap-6 md:gap-16 items-center'>
             <div>
-                <Image height={600} width={600} alt='processimage' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
+                <Image height={600} width={600} alt='process_image' src={'https://i.postimg.cc/GhR0D9J8/process.webp'} ></Image>
             </div>
             <div>
                 <h2 className='text-theme text-6xl'>Process</h2>
-                <p><span className='text-theme'>As a</span> full-service architectural firm, Measured is engaged in every phase of a project from concept to completion, enabling the creation of environments (building, interiors and landscape) that facilitate the desires of clients. Our goal is to design a backdrop for your lives where your experience of it is exactly what you imagined it would be at the outset.</p>
+                <p className='my-6'><span className='text-theme'>As a</span> full-service architectural firm, Measured is engaged in every phase of a project from concept to completion, enabling the creation of environments (building, interiors and landscape) that facilitate the desires of clients. Our goal is to design a backdrop for your lives where your experience of it is exactly what you imagined it would be at the outset.</p>
                 <a text-theme className='' href="">Read more</a>
             </div>
         </div>
