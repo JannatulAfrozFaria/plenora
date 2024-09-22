7163d7f2b8866e40a271fc60a7712090b304f969
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 04:30:24 +0600

    heading added to Process

--- src/components/HomePage/Process.jsx
+++ src/components/HomePage/Process.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Process = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-2 gap-6 md:gap-16'>
+            <div></div>
+            <div>
+                <h2>Process</h2>
+                <p></p>
+            </div>
         </div>
     );
 };
