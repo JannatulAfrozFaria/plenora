06a962f92e5ec533ec57a278a9901243d81ebe2a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 06:26:10 +0600

    bg image link replaced

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -9,9 +9,7 @@ const WhatWeDo = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 relative w-full h-[90vh] bg-no-repeat bg-cover"
                     style={{
-                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${banner.image})`,
-
-                    }}>
+                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url('https://i.postimg.cc/ZYvpyK9Q/back-ash.png')` }}>
                 <div className="grid"></div>
                
             </div>
