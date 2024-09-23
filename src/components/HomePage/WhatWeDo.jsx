346bf560a46ef86d93be42752aaea0fac51cd32d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 06:24:29 +0600

    background image set

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -7,7 +7,11 @@ const WhatWeDo = () => {
                 <h1 className='text-gray font-bold'>What we do</h1>
                 <h2 className="text-theme text-3xl md:text-6xl">The best Services We Provide</h2>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="grid grid-cols-1 md:grid-cols-3 relative w-full h-[90vh] bg-no-repeat bg-cover"
+                    style={{
+                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${banner.image})`,
+
+                    }}>
                 <div className="grid"></div>
                
             </div>
