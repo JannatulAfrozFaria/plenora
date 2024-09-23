2cf513c16d57251ac01cc030ab9ae2714b9e5d42
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 04:24:06 +0600

    image 6 replaced

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -39,7 +39,7 @@ const WhatWeDo = () => {
             "id": 6,
             "heading": "Custom Design Plan",
             "description": "Get personalized design plans that suit your unique vision and lifestyle needs perfectly.",
-            "image": "https://i.postimg.cc/5t5kJSJw/iconb3.png"
+            "image": "https://i.postimg.cc/mghP4wFK/iconb3-removebg-preview.png"
           }
         ]
       
