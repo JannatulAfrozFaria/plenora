54aa21e4adb2af75f69b705992ebfebabc65642d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 04:28:22 +0600

    Title component created

---
+++ src/components/utilityComponents/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = ({heading,subHeading}) => {
+    return (
+        <div>
+             <div className='text-center mb-6'>
+                <h1 className='text-gray font-bold mb-2'>{subHeading}</h1>
+                <h2 className="text-theme text-2xl md:text-5xl">{heading}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Title;
