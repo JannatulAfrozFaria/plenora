ad668736e9b84e23f9d6eed0234c4b224385e0b3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 04:42:29 +0600

    grid layout created

--- src/components/HomePage/WhyChooseUs.jsx
+++ src/components/HomePage/WhyChooseUs.jsx
@@ -5,6 +5,10 @@ const WhyChooseUs = () => {
     return (
         <div>
             <Title heading={'Why choose Plenora Architects'} subHeading={'Why choose Us'} ></Title>
+            <div className='grid grid-cols-2 gap-12 items-center'>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
