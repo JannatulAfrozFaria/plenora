39a19ba7d8cc7fbb1c97a2aedbbab787fbb7c4f4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 04:31:36 +0600

    title imported to why choose us section

--- src/components/HomePage/WhyChooseUs.jsx
+++ src/components/HomePage/WhyChooseUs.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Title from '../utilityComponents/Title';
 
 const WhyChooseUs = () => {
     return (
         <div>
-            
+            <Title heading={'What we do'} subHeading={'The best Services We Provide'} ></Title>
         </div>
     );
 };
