8e02bae6f499a077165f71f8fecc2d9254549f99
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 05:40:29 +0600

    working strategy heading added

--- src/components/HomePage/WorkingProcess.jsx
+++ src/components/HomePage/WorkingProcess.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WorkingProcess = () => {
     return (
         <div>
-            
+             <Title heading={'Our Working Strategy'} subHeading={'Working Process'} ></Title>
         </div>
     );
 };
