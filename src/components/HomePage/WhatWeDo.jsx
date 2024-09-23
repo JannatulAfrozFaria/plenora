d8790d04ebc4d5e631b0479b77a42e3777962f97
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 06:29:26 +0600

    gap set up for grid

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -10,10 +10,12 @@ const WhatWeDo = () => {
             <div className="relative w-full h-[90vh] bg-no-repeat bg-cover"
                     style={{
                         backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url('https://i.postimg.cc/ZYvpyK9Q/back-ash.png')` }}>
-                <div className="grid grid-cols-1 md:grid-cols-3 absolute top-6 ">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6 absolute top-6 ">
                     <div className="grid grid-cols-2">
                         <div></div>
-                        <div></div>
+                        <div>
+                            <h2>Home Interior Design</h2>
+                        </div>
                     </div>
                 </div>
                
