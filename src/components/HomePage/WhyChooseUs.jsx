039b9200238ad8ec5e514f552a822e9b3e5ed3c3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 04:33:35 +0600

    heading added to choose section

--- src/components/HomePage/WhyChooseUs.jsx
+++ src/components/HomePage/WhyChooseUs.jsx
@@ -4,7 +4,7 @@ import Title from '../utilityComponents/Title';
 const WhyChooseUs = () => {
     return (
         <div>
-            <Title heading={'Why choose Us'} subHeading={'Why choose Us'} ></Title>
+            <Title heading={'Why choose Plenora Architects'} subHeading={'Why choose Us'} ></Title>
         </div>
     );
 };
