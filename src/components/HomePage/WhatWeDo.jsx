f97dbc5d7477f302c043cc2ef150c450418e8f84
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-23 06:20:29 +0600

    grid created for both devices

--- src/components/HomePage/WhatWeDo.jsx
+++ src/components/HomePage/WhatWeDo.jsx
@@ -7,6 +7,10 @@ const WhatWeDo = () => {
                 <h1 className='text-gray font-bold'>What we do</h1>
                 <h2 className="text-theme text-3xl md:text-6xl">The best Services We Provide</h2>
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-3">
+                <div className="grid"></div>
+               
+            </div>
         </div>
     );
 };
