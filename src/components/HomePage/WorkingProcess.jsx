9cd1056dbe497c2fa3a01e00f022e45c6024d200
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 17:44:17 +0600

    Statistics Component created

--- src/components/HomePage/WorkingProcess.jsx
+++ src/components/HomePage/WorkingProcess.jsx
@@ -8,40 +8,41 @@ const WorkingProcess = () => {
             id: 1,
             title: "Visit Project",
             description: "We conduct an initial site visit to understand the project’s requirements and scope.",
-            image: "https://i.ibb.co/Wxn4R8q/p1.png"
+            image: "https://i.ibb.co/LRH1n80/p1a.png"
         },
         {
             id: 2,
             title: "Planning Design",
             description: "Developing a comprehensive design plan based on client preferences and project goals.",
-            image: "https://i.ibb.co/0GM1TpC/p2.png"
+            image: "https://i.ibb.co/kDKCd62/p2-a.png"
         },
         {
             id: 3,
             title: "Project Sketch",
             description: "Creating detailed sketches and blueprints to visualize the proposed architectural solutions.",
-            image: "https://i.ibb.co/WWSfjQ1/p3.png"
+            image: "https://i.ibb.co/hBrs7Fx/p3-a.png"
         },
         {
             id: 4,
             title: "Start Working",
             description: "Execution of the approved design with close monitoring of progress and quality.",
-            image: "https://i.ibb.co/6tTr5b1/p4.png"
+            image: "https://i.ibb.co/1qnx7Xz/p4-a.png"
         }
     ];
     
     return (
         <div>
              <Title heading={'Our Working Strategy'} subHeading={'Working Process'} ></Title>
-             <div className="grid grid-cols-4 gap-6">
-             {Strategies.map(strategy=>
+             <div className="grid  grid-cols-1 md:grid-cols-4 gap-6">
+                    {Strategies.map((strategy,index)=>
                             <div key={strategy.id} className="grid grid-cols-1 gap-6 text-center">
-                            <div className='flex justify-center'>
-                                <Image width={150} height={160} alt='strategy-info' src={strategy.image}></Image>
+                            <div className='flex justify-center relative'>
+                                <Image className='p-6 rounded_custom  ' width={150} height={160} alt='strategy-info' src={strategy.image}></Image>
+                                <div className="bg-orange-500 rounded_custom py-1 px-3 absolute  top-4 left-24 md:left-16 text-white  ">{index+1} </div>
                             </div>
                             <div  className=''>
                                 <h2 className='text-theme text-md  md:text-xl font-bold'>{strategy.title}</h2>
-                                <p className=' text-sm md:text-base text-gray my-2 text-center'> {strategy.description} </p>
+                                <p className=' text-sm md:text-base text-gray my-2 text-center w-4/5 md:w-5/6 mx-auto'> {strategy.description} </p>
                             </div>
                         </div>
                         )}
