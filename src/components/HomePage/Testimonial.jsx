6158dc6c55b8a7d533848c0cafb2a169b2a8ec6f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 03:26:55 +0600

    description added to Happy Customers Section

--- src/components/HomePage/Testimonial.jsx
+++ src/components/HomePage/Testimonial.jsx
@@ -5,6 +5,7 @@ const Testimonial = () => {
     return (
         <div>
             <Title heading={'Our Happy Customers'} subHeading={'Testimonial'} ></Title>
+            <p className='text-gray'>See what our happy clients are saying! Discover how we’ve transformed spaces into beautiful, functional designs they love.</p>
         </div>
     );
 };
