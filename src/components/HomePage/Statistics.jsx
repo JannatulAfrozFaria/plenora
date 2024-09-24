48403c76104aad33b2b35ae6461f2f53c9f5e65f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-24 17:44:46 +0600

    Statistics Json added

--- src/components/HomePage/Statistics.jsx
+++ src/components/HomePage/Statistics.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 
 const Statistics = () => {
+    const Statistics = [
+        {
+            id: 1,
+            title: "Years of Experience",
+            number: "15+",
+            image: "https://example.com/images/years-experience.jpg"
+        },
+        {
+            id: 2,
+            title: "Success Projects",
+            number: "600+",
+            image: "https://example.com/images/success-projects.jpg"
+        },
+        {
+            id: 3,
+            title: "Team Members",
+            number: "40+",
+            image: "https://example.com/images/team-members.jpg"
+        },
+        {
+            id: 4,
+            title: "Clients Satisfaction",
+            number: "500+",
+            image: "https://example.com/images/client-satisfaction.jpg"
+        }
+    ];
+    
     return (
         <div>
             
