da7e727ff20b6ac9692b53901452ca02cac12d58
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 03:31:16 +0600

    happy cutomer section grid layout created

--- src/components/HomePage/Testimonial.jsx
+++ src/components/HomePage/Testimonial.jsx
@@ -6,6 +6,13 @@ const Testimonial = () => {
         <div>
             <Title heading={'Our Happy Customers'} subHeading={'Testimonial'} ></Title>
             <p className='text-gray'>See what our happy clients are saying! Discover how we’ve transformed spaces into beautiful, functional designs they love.</p>
+            <div key={member.id} className="relative w-full h-[30vh] bg-no-repeat bg-cover" style={{
+                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${member.image})`
+                    }}>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {}
+                    </div>
+            </div>
         </div>
     );
 };
