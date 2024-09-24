edf4f46cdfca28f2d31fb580a17d47580261ad6a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 04:35:53 +0600

    bg image added

--- src/components/HomePage/Testimonial.jsx
+++ src/components/HomePage/Testimonial.jsx
@@ -38,9 +38,9 @@ const Testimonial = () => {
             <Title heading={'Our Happy Customers'} subHeading={'Testimonial'} ></Title>
             <p className='text-gray'>See what our happy clients are saying! Discover how we’ve transformed spaces into beautiful, functional designs they love.</p>
             <div  className="relative w-full h-[30vh] bg-no-repeat bg-cover" style={{
-                backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${member.image})`
+                backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url('https://i.ibb.co/RDHBPW1/e1.jpg')`
             }}>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 absolute top-3/5 left-1/5">
                     {customers.slice(0, 3).map(customer =>
                         <div key={customer.id} className="grid grid-cols-1 p-8">
                             <div className='flex gap-3'>
