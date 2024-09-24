6f1f476702f014311d189b8d90309bedffb5f420
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 03:24:22 +0600

    Title added to testimonial

--- src/components/HomePage/Testimonial.jsx
+++ src/components/HomePage/Testimonial.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Title from '../utilityComponents/Title';
 
 const Testimonial = () => {
     return (
         <div>
-            
+            <Title></Title>
         </div>
     );
 };
