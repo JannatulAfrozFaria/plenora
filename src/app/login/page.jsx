9ddcb06bee999949db9f552faa34a9bfc3da5446
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 03:38:58 +0600

    columns added to login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const page = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-            
+            <div></div>
+            <div></div>
         </div>
     );
 };
