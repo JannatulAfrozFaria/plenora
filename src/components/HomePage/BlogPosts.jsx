fdabe3cd2c2a79e666e9f987cdbde30898f8ee32
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 01:01:15 +0600

    Title imported to BlogPosts Component

--- src/components/HomePage/BlogPosts.jsx
+++ src/components/HomePage/BlogPosts.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Title from '../utilityComponents/Title';
 
 const BlogPosts = () => {
     return (
         <div>
-            
+            <Title></Title>
         </div>
     );
 };
