ca02b3b17f603679e360578b4b34fecae71c2020
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 04:01:30 +0600

    background image set

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full h-[50vh] md:h-[90vh] bg-no-repeat bg-cover"
+            style={{
+                backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url('https://i.ibb.co/NFqHwVQ/orange.jpg')`,
+            }}
+        >
             <div>
                 <Image width={500} height={800} alt='loginImage' src={'https://i.ibb.co/KXc8VLX/login.jpg'}></Image>
             </div>
