1c69b9d068b35014a2126b44518e9d23d3fd0efe
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 03:34:30 +0600

    grid layout setup for login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+            
+        </div>
+    );
+};
+
+export default page;
