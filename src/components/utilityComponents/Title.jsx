409157b3c17960a7898287ee2a520c0aa8e73128
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 03:31:55 +0600

    animation updated

--- src/components/utilityComponents/Title.jsx
+++ src/components/utilityComponents/Title.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Title = ({heading,subHeading}) => {
     return (
-        <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="3000">
+        <div >
              <div className='text-center mb-6'>
-                <h1 className='text-gray font-bold mb-2'>{subHeading}</h1>
-                <h2 className="text-theme text-2xl md:text-5xl mb-4 md:mb-6">{heading}</h2>
+                <h1 data-aos="fade-right" data-aos-delay="500" data-aos-duration="2000" className='text-gray font-bold mb-2'>{subHeading}</h1>
+                <h2 data-aos="fade-left" data-aos-delay="1000" data-aos-duration="3000" className="text-theme text-2xl md:text-5xl mb-4 md:mb-6">{heading}</h2>
             </div>
         </div>
     );
