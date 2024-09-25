c103dc34cb9acc39e93ed972ad4d83c58852a4f2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 01:04:13 +0600

    Title style Updated

--- src/components/utilityComponents/Title.jsx
+++ src/components/utilityComponents/Title.jsx
@@ -5,7 +5,7 @@ const Title = ({heading,subHeading}) => {
         <div>
              <div className='text-center mb-6'>
                 <h1 className='text-gray font-bold mb-2'>{subHeading}</h1>
-                <h2 className="text-theme text-2xl md:text-5xl">{heading}</h2>
+                <h2 className="text-theme text-2xl md:text-5xl mb-4 md:mb-6">{heading}</h2>
             </div>
         </div>
     );
