24a308ada5496b0f7ad6b67d326c0289718f4c5c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 23:37:48 +0600

    row2 image 1 replaced

--- src/components/HomePage/Marquee.jsx
+++ src/components/HomePage/Marquee.jsx
@@ -13,7 +13,7 @@ const Marquee = () => {
       ];
       
       const row2 = [
-        { id: 9, firmName: "Crafted", firmType: "Art & Design", image: "https://example.com/images/crafted.jpg" },
+        { id: 9, firmName: "Crafted", firmType: "Art & Design", image: "https://i.postimg.cc/6qQ9DSw7/logo3-removebg-preview.png" },
         { id: 10, firmName: "Harmony", firmType: "Art & Design", image: "https://example.com/images/harmony.jpg" },
         { id: 11, firmName: "Echo", firmType: "Art & Design", image: "https://example.com/images/echo.jpg" },
         { id: 12, firmName: "Illusion", firmType: "Art & Design", image: "https://example.com/images/illusion.jpg" },
