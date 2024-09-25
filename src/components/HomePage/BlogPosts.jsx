bf788a372d9c2d705834759616f04ee54a9c83a3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 01:05:21 +0600

    grid Layout set for blog

--- src/components/HomePage/BlogPosts.jsx
+++ src/components/HomePage/BlogPosts.jsx
@@ -5,6 +5,9 @@ const BlogPosts = () => {
     return (
         <div>
             <Title subHeading={'Articles & Blogs'} heading={'Our Latest Blog Posts'}></Title>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+
+            </div>
         </div>
     );
 };
