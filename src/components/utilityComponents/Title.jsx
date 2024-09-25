f928962e7234460ec119d189ae1fbc4f85acdb38
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 03:30:53 +0600

    animation added to Title

--- src/components/utilityComponents/Title.jsx
+++ src/components/utilityComponents/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({heading,subHeading}) => {
     return (
-        <div>
+        <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="3000">
              <div className='text-center mb-6'>
                 <h1 className='text-gray font-bold mb-2'>{subHeading}</h1>
                 <h2 className="text-theme text-2xl md:text-5xl mb-4 md:mb-6">{heading}</h2>
