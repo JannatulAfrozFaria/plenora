490539d0e3c5d75b2dbbe38bf2f169b47e7160ce
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 02:33:24 +0600

    flex gap set

--- src/components/HomePage/NewsLetter.jsx
+++ src/components/HomePage/NewsLetter.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 const NewsLetter = () => {
     return (
         <div className='theme-bg'>
-            <div className='w-5/6 mx-auto'>
+            <div className='w-5/6 mx-auto flex gap-16 justify-between'>
                 <div>
                     <p className="text-base md:text-xl text-black">Stay in the loop, subscribe to our</p>
                     <h1 className="text-3xl md:text-6xl">Newsletter</h1>
                 </div>
-                <div></div>
+                <div className='text-white'>
+                    <h1>Your Name</h1>
+                    <h1>Your email address</h1>
+                </div>
             </div>
         </div>
     );
