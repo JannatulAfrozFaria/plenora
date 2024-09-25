cc8b41dddf465e29478ecd44c72fe9a2ce170c26
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 03:55:13 +0600

    image added to layout

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import React from 'react';
 
 const page = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-            <div></div>
+            <div>
+                <Image width={500} height={800} alt='loginImage' src={'https://i.ibb.co/KXc8VLX/login.jpg'}></Image>
+            </div>
             <div></div>
         </div>
     );
