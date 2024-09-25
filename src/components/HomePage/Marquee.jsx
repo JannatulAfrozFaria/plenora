fe96554af7dee5aa90576304a48bac6a4e39236b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-25 22:58:44 +0600

    row 1 and row 2 array added

--- src/components/HomePage/Marquee.jsx
+++ src/components/HomePage/Marquee.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
-
+import styled,{keyframes,css} from "styled-components/styled-components"
 const Marquee = () => {
+    const row1 = [
+        { id: 1, firmName: "Skyline", firmType: "Art & Design", image: "https://example.com/images/skyline.jpg" },
+        { id: 2, firmName: "UrbanMinds", firmType: "Art & Design", image: "https://example.com/images/urbanminds.jpg" },
+        { id: 3, firmName: "Visionary", firmType: "Art & Design", image: "https://example.com/images/visionary.jpg" },
+        { id: 4, firmName: "Inspire", firmType: "Art & Design", image: "https://example.com/images/inspire.jpg" },
+        { id: 5, firmName: "Canvas", firmType: "Art & Design", image: "https://example.com/images/canvas.jpg" },
+        { id: 6, firmName: "AvantGarde", firmType: "Art & Design", image: "https://example.com/images/avantgarde.jpg" },
+        { id: 7, firmName: "Fusion", firmType: "Art & Design", image: "https://example.com/images/fusion.jpg" },
+        { id: 8, firmName: "ModernMuse", firmType: "Art & Design", image: "https://example.com/images/modernmuse.jpg" }
+      ];
+      
+      const row2 = [
+        { id: 9, firmName: "Crafted", firmType: "Art & Design", image: "https://example.com/images/crafted.jpg" },
+        { id: 10, firmName: "Harmony", firmType: "Art & Design", image: "https://example.com/images/harmony.jpg" },
+        { id: 11, firmName: "Echo", firmType: "Art & Design", image: "https://example.com/images/echo.jpg" },
+        { id: 12, firmName: "Illusion", firmType: "Art & Design", image: "https://example.com/images/illusion.jpg" },
+        { id: 13, firmName: "BoldBrush", firmType: "Art & Design", image: "https://example.com/images/boldbrush.jpg" },
+        { id: 14, firmName: "Phoenix", firmType: "Art & Design", image: "https://example.com/images/phoenix.jpg" },
+        { id: 15, firmName: "Elemental", firmType: "Art & Design", image: "https://example.com/images/elemental.jpg" },
+        { id: 16, firmName: "CreativeEdge", firmType: "Art & Design", image: "https://example.com/images/creativeedge.jpg" }
+      ];
+      
     return (
         <div>
             
