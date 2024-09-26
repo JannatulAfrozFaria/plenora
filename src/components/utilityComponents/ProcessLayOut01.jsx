733eb15113d0b90e01f76b0984aac06ffe2f598a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 20:17:15 +0600

    ProcessLayOut 1 created

---
+++ src/components/utilityComponents/ProcessLayOut01.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ProcessLayOut01 = () => {
+    return (
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div>
+                    <Image height={900} width={800} alt='process_image' src={'https://i.ibb.co/C8GDN41/process01.webp'} ></Image>
+                </div>
+                <div className='grid grid-col-1'>
+                    <div className='flex'>
+                        <div>
+                            <Image height={400} width={396} alt='process_image' src={'https://i.ibb.co/f9y5CG5/process01a.webp'} ></Image>
+                        </div>
+                        <div>
+                            <Image height={500} width={500} alt='process_image' src={'https://i.ibb.co/6tHcVj4/process01b.webp'} ></Image>
+                        </div>
+                    </div>
+                    <div className='w-4/5 mx-auto p-10 md:p-0'>
+                        <h1 className="text-theme font-black text-7xl ">Discover</h1>
+                        <p className='my-6 text-justify md:text-left'><span className='text-theme font-semibold' > During discovery,</span> we work closely with clients to gain an understanding of your desires and needs, both the philosophical (how do you want to live?) and the practical (how many bedrooms and bathrooms?). We strive to learn what is important to you and work to protect that throughout the life of the project.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProcessLayOut01;
