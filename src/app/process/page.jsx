c78f9e5f34b532507eb3a974ce9b41ea85b0590c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 19:58:25 +0600

    process01 image added

--- src/app/process/page.jsx
+++ src/app/process/page.jsx
@@ -36,7 +36,7 @@ const page = () => {
                             <Image height={400} width={396} alt='process_image' src={'https://i.ibb.co/f9y5CG5/process01a.webp'} ></Image>
                         </div>
                         <div>
-                            <Image height={500} width={500} alt='process_image' src={'https://i.postimg.cc/G2chQKzt/featured-1.webp'} ></Image>
+                            <Image height={500} width={500} alt='process_image' src={'https://i.ibb.co/6tHcVj4/process01b.webp'} ></Image>
                         </div>
                     </div>
                     <div className='w-4/5 mx-auto p-10 md:p-0'>
