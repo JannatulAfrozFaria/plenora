efdde385f1dc229c1b0dd499973cd24de6661260
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 20:21:54 +0600

    Template design complete

--- src/components/utilityComponents/ProcessLayOut01.jsx
+++ src/components/utilityComponents/ProcessLayOut01.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProcessLayOut01 = ({coverImage,gridImg1,gridImg2, heading, description}) => {
+const ProcessLayOut01 = ({coverImage,gridImg1,gridImg2, heading, themeText, grayText}) => {
     return (
         <div>
             <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -17,8 +17,8 @@ const ProcessLayOut01 = ({coverImage,gridImg1,gridImg2, heading, description}) =
                         </div>
                     </div>
                     <div className='w-4/5 mx-auto p-10 md:p-0'>
-                        <h1 className="text-theme font-black text-7xl ">Discover</h1>
-                        <p className='my-6 text-justify md:text-left'><span className='text-theme font-semibold' > During discovery,</span> we work closely with clients to gain an understanding of your desires and needs, both the philosophical (how do you want to live?) and the practical (how many bedrooms and bathrooms?). We strive to learn what is important to you and work to protect that throughout the life of the project.</p>
+                        <h1 className="text-theme font-black text-7xl ">{heading}</h1>
+                        <p className='my-6 text-justify md:text-left'><span className='text-theme font-semibold' >{themeText}</span> {grayText}</p>
                     </div>
                 </div>
             </div>
