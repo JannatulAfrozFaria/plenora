9cf6b7e7431871c7f93ebbbb259745dc460f1910
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 04:22:43 +0600

    margin added

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-5'>
+        <div className='w-5/6 mx-auto grid grid-cols-1 md:grid-cols-5'>
             <div className='col-span-4 text-[130px] md:text-[200px]'>
                 <h1 className='text-gray-300 '>Start the Conversation</h1>
                 <h1 className='text-theme'>T: 604.737.0235
