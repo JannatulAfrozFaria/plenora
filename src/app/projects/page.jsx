317984269286a3e7a1074a83ee3aca3570c5efe4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 02:44:20 +0600

    Cover div set up

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -15,6 +15,27 @@ const page = () => {
                     
                 </div>
             </div>
+            <div>
+                {ProjectsInfo.slice(13,14).map(project=>
+                     <div key={project.id} className='grid grid-cols-1 md:grid-cols-2  items-center overflow-hidden'>
+                     <div className="carousel-item relative w-full h-full bg-no-repeat bg-cover transition-transform duration-500 hover:scale-110"
+                         style={{
+                             backgroundImage: ` url(${project.image})`,
+                         }}>
+                             <div className="absolute top-[140px] -right-7 rotate-45 text-3xl">
+                                     <Image width={50} height={50} src={'https://i.ibb.co/m6ZcvjR/tri-removebg-preview.png'} alt='triangle'></Image>
+                             </div>
+                     </div>
+                     <div className='text-gray pl-10 pr-8 py-6'>
+                         <h1 className='text-theme text-4xl font-bold'>{project.name} </h1>
+                         <div className="mt-10">
+                             <h1> <span className='font-bold'>Category:</span> {project.category} </h1>
+                             <h1> <span className='font-bold'>Description: <br /> </span> {project.description} </h1>
+                         </div>
+                     </div>
+                 </div>
+                )}
+            </div>
             <div className=' grid grid-cols-2'>
                         {ProjectsInfo.slice(0,2).map(project=>
                             <ProjectCard1 key={project.id} project ={project} ></ProjectCard1>
