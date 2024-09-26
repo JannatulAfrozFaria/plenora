686e83b5b026ae938b6120725b88848a426fe98d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 17:26:22 +0600

    description added

--- src/app/process/page.jsx
+++ src/app/process/page.jsx
@@ -15,13 +15,12 @@ const page = () => {
             {/* SERVICE----INTRO */}
             <div className="w-5/6 mx-auto my-12">
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-16 items-center'>
+                    
+                    <div className='w-5/6 md:w-full mx-auto'>
+                        <p className='my-6  text-justify md:text-left'> <span className='text-theme font-semibold'> As A </span>full-service architectural firm, Measured is engaged in every phase of a project from concept to completion, enabling the creation of environments (building, interiors and landscape) that facilitate the desires of clients. Our process ensures this. </p>
+                    </div>
                     <div>
                         <Image height={900} width={900} alt='process_image' src={'https://i.ibb.co/cJQfVGX/About-1.webp'} ></Image>
-                    </div>
-                    <div className='w-5/6 md:w-full mx-auto'>
-                        {/* <h2 className='text-theme text-6xl font-semibold'>Process</h2> */}
-                        <p className='my-6  text-justify md:text-left'> <span className='text-theme font-semibold'> Plenora Architects </span>is a Vancouver-based studio practice focused on modern design, interiors and landscapes. From our inception in 2007, we have delivered exceptional public and private environments that are stimulating to occupy and fundamental to their surroundings. As a full-service firm, Measured is present on projects from start to finish, ensuring that the ideas conceived in the beginning with clients are manifest at the end. We achieve this through close collaboration with all members of the project team, including consultants, contractors, trades and artisans.</p>
-                        {/* <a className='text-theme font-bold' href="">Read more</a> */}
                     </div>
                 </div>
             </div>
