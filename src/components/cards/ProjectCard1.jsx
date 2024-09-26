4643e1c40e6d914c2b91564af22fde30e061c956
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 00:46:33 +0600

    date and phase added to card

--- src/components/cards/ProjectCard1.jsx
+++ src/components/cards/ProjectCard1.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 const ProjectCard1 = ({project}) => {
-    const {id,name, category,description,image} = project;
+    const {id,name, category,description,image,handOverDate,phase} = project;
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 items-center'>
             <div>
@@ -10,7 +10,12 @@ const ProjectCard1 = ({project}) => {
             </div>
             <div className='text-gray'>
                 <h1 className='text-theme text-4xl font-bold'>{name} </h1>
-                <h1> <span className='font-bold'>Category:</span> {category} </h1>
+                <div className="mt-12">
+                    <h1> <span className='font-bold'>Category:</span> {category} </h1>
+                    <h1> <span className='font-bold'>Description: <br /> </span> {description} </h1>
+                    <h1> <span className='font-bold'>Current Phase:</span> {phase} </h1>
+                    <h1> <span className='font-bold'>Hand-over Date:</span> {handOverDate} </h1>
+                </div>
             </div>
         </div>
     );
