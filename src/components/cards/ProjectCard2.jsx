4d71d196085e8876ef22c522e0e6d600290080d1
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 01:31:45 +0600

    project 2 layout set up

--- src/components/cards/ProjectCard2.jsx
+++ src/components/cards/ProjectCard2.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const ProjectCard2 = () => {
+const ProjectCard2 = ({project}) => {
+    const {id,name, category,description,image,handOverDate,phase} = project;
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2  items-center'>
+            <div className="carousel-item relative w-full h-full bg-no-repeat bg-cover"
+                style={{
+                    backgroundImage: ` url(${image})`,
+                }}>
+                    <div className="absolute top-[140px] -right-7 rotate-45 bg-white w-30 h-30 text-3xl pt-1 text-white ">hel</div>
+            </div>
+            <div className='text-gray pl-10 pr-8 py-6'>
+                <h1 className='text-theme text-4xl font-bold'>{name} </h1>
+                <div className="mt-10">
+                    <h1> <span className='font-bold'>Category:</span> {category} </h1>
+                    <h1> <span className='font-bold'>Description: <br /> </span> {description} </h1>
+                </div>
+            </div>
         </div>
     );
 };
