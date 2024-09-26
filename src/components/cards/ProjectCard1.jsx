fb3e5a99551785f3d9492b57d64298f8f49f10d1
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 00:39:03 +0600

    NextImage added to Card

--- src/components/cards/ProjectCard1.jsx
+++ src/components/cards/ProjectCard1.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ProjectCard1 = ({project}) => {
     const {id,name, category,description,image} = project;
     return (
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div>
+                <Image></Image>
+            </div>
             
         </div>
     );
