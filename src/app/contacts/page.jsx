9740d68c55892e1bcec917a55e088b9a5083bc86
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 03:59:50 +0600

    adress added

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -6,10 +6,13 @@ const page = () => {
             <div className='col-span-4 text-[130px] md:text-[200px]'>
                 <h1 className='text-gray-300 '>Start the Conversation</h1>
                 <h1 className='text-theme'>T: 604.737.0235
-                build@plenora.ca</h1>
+                    build@plenora.ca</h1>
             </div>
             <div className='col-span-1'>
                 <h1 className="text-theme">Plenora Architects Ltd.</h1>
+                <p> #410-1639 West 2nd Avenue <br />
+                    Vancouver, BC <br />
+                    Canada V6J 1H3</p>
             </div>
         </div>
     );
