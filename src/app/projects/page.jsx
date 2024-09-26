191862d67e6a0baf13b9896ece06108ef0b544db
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 23:38:46 +0600

    Heading Added to Projects Page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            
+            <div className="w-5/6 mx-auto">
+                <div className='text-gray-400 text-[130px] md:text-[260px]'>Work</div>
+                <div></div>
+            </div>
         </div>
     );
 };
