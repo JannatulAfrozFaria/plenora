4df3d7310517ba1cc51e26fddd569783fe56cf3f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 03:51:38 +0600

    contacts page layout created

---
+++ src/app/contacts/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-5'>
+            <div className='col-span-4'></div>
+            <div className='col-span-1'></div>
+        </div>
+    );
+};
+
+export default page;
