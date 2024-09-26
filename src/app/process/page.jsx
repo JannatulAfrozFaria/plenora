61c5403426b57af9828ed930b697ee3e2501c67c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-26 17:20:09 +0600

    heading of banner updated

--- src/app/process/page.jsx
+++ src/app/process/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
                     backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.3),rgba(0,0,0,0.2)), url('https://i.postimg.cc/GhR0D9J8/process.webp')`,
                 }}>
                 <div>
-                    <h1 className=" absolute p-24 top-20  text-9xl font-black text-white">Our <br /> <span className='text-theme'>Studio</span></h1>
+                    <h1 className=" absolute p-24 top-20  text-9xl font-black text-white">Our <br /> <span className='text-theme'>Process</span></h1>
                 </div>
             </div>
         </div>
