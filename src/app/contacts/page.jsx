00cb9c1e0ed1b6254f03fdc3056d2f4fb493a1ff
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 03:57:17 +0600

    Banner left part info added

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const page = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-5'>
-            <div className='col-span-4'>
-                <h1 className='text-gray-300 text-[130px] md:text-[200px]'>Start the Conversation</h1>
+            <div className='col-span-4 text-[130px] md:text-[200px]'>
+                <h1 className='text-gray-300 '>Start the Conversation</h1>
+                <h1 className='text-theme'>T: 604.737.0235
+                build@plenora.ca</h1>
             </div>
             <div className='col-span-1'></div>
         </div>
