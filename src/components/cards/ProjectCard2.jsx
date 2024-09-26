b2b170b171487fa48da7335f3c070d6f311e9595
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 02:16:04 +0600

    grid style updated in project card 2

--- src/components/cards/ProjectCard2.jsx
+++ src/components/cards/ProjectCard2.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectCard2 = ({project}) => {
     const {id,name, category,description,image,handOverDate,phase} = project;
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2  items-center'>
+        <div className='grid grid-cols-1 md:grid-cols-2  items-center overflow-hidden'>
             <div className='text-gray pr-10 pl-8 py-6 text-right'>
                 <h1 className='text-theme text-4xl font-bold'>{name} </h1>
                 <div className="mt-10">
