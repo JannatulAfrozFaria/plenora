a90e1685703104d7ff55863f3cd7861ab5832cca
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 01:30:49 +0600

    padding-y updated in card1

--- src/components/cards/ProjectCard1.jsx
+++ src/components/cards/ProjectCard1.jsx
@@ -10,15 +10,12 @@ const ProjectCard1 = ({project}) => {
                     backgroundImage: ` url(${image})`,
                 }}>
                     <div className="absolute top-[140px] -right-7 rotate-45 bg-white w-30 h-30 text-3xl pt-1 text-white ">hel</div>
-                {/* <Image width={500} height={500} alt='projectImage' src={image} ></Image> */}
             </div>
-            <div className='text-gray pl-10 pr-8'>
+            <div className='text-gray pl-10 pr-8 py-6'>
                 <h1 className='text-theme text-4xl font-bold'>{name} </h1>
-                <div className="mt-12">
+                <div className="mt-10">
                     <h1> <span className='font-bold'>Category:</span> {category} </h1>
                     <h1> <span className='font-bold'>Description: <br /> </span> {description} </h1>
-                    {/* <h1> <span className='font-bold'>Current Phase:</span> {phase} </h1>
-                    <h1> <span className='font-bold'>Hand-over Date:</span> {handOverDate} </h1> */}
                 </div>
             </div>
         </div>
