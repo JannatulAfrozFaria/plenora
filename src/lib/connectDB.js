a05b6a3847b4cfab534322c99cf529afb6297581
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-27 17:35:23 +0600

    client added to connectDB

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -2,7 +2,13 @@ let db;
 export const connectDB = async()=>{
     if(db) return db;
     try {
-        
+        const client = new MongoClient(uri, {
+            serverApi: {
+              version: ServerApiVersion.v1,
+              strict: true,
+              deprecationErrors: true,
+            }
+          });
     } catch (error) {
         console.log(error);
     }
