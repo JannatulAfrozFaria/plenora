75031cd45cb08a5dd0f1d8114a2b5537e843586f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-30 00:41:57 +0600

    animate opacity attribute added

--- src/app/PageWrapper.jsx
+++ src/app/PageWrapper.jsx
@@ -3,7 +3,10 @@ import { motion, AnimatePresence } from "framer-motion";
 export const PageWrapper = ({children})=>(
     <>
         <AnimatePresence>
-            <motion.div>
+            <motion.div 
+            initial={{opacity:0, y: 15}}
+            animate={{opacity: 1, y: 0}}
+             >
                 {children}
             </motion.div>
         </AnimatePresence>
