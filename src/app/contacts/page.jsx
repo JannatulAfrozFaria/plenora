ec8a6d5a73062aba14aab1bce01bebbd2e1dd9a5
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-30 00:56:02 +0600

    PageWarapper imported to contacts page

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -5,6 +5,7 @@ import { FaFacebookF } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { PiGithubLogoFill } from "react-icons/pi";
+import { PageWrapper } from '../PageWrapper';
 
 const page = () => {
     const TeamMembers = [
@@ -107,68 +108,70 @@ const page = () => {
     ];
     return (
         <div>
-            <div className='w-5/6 mx-auto grid grid-cols-1 md:grid-cols-5  gap-24'>
-                <div className='col-span-4 text-[40px] md:text-[90px] '>
-                    <h1 className='text-gray-300 '>Start the  <br />
-                        Conversation</h1>
-                    <h1 className='text-theme font-bold text-[40px] md:text-[100px]'>T: 604.737.0235
-                        build@plenora.ca</h1>
-                </div>
-                <div className='col-span-4 md:col-span-1 text-justify text-gray grid grid-cols-1 gap-6 items-center'>
-                    <div>
-                        <h1 className="text-theme font-bold">Plenora Architects Ltd.</h1>
-                        <p> #410-1639 West 2nd Avenue <br />
-                            Vancouver, BC <br />
-                            Canada V6J 1H3</p>
-                        <a href="https://www.google.ca/maps/place/1639+W+2nd+Ave,+Vancouver,+BC+V6J+1H3/@49.2699915,-123.1440888,17z/data=!3m1!4b1!4m5!3m4!1s0x548673c96f4a8f7b:0x89c8b0ca78f7ea82!8m2!3d49.269988!4d-123.1419001">Get Directions</a>
+            <PageWrapper>
+                <div className='w-5/6 mx-auto grid grid-cols-1 md:grid-cols-5  gap-24'>
+                    <div className='col-span-4 text-[40px] md:text-[90px] '>
+                        <h1 className='text-gray-300 '>Start the  <br />
+                            Conversation</h1>
+                        <h1 className='text-theme font-bold text-[40px] md:text-[100px]'>T: 604.737.0235
+                            build@plenora.ca</h1>
                     </div>
-                    <div>
-                        <h1 className="text-theme font-bold">Follow us</h1>
-                        <a className='hover:text-theme hover:underline' href="https://www.instagram.com/">Instagram</a>
-                    </div>
-                    <div>
-                        <h1 className="text-theme font-bold">Press inquiries</h1>
-                        <p> #410-1639 West 2nd Avenue <span> <a className=' text-gray hover:text-theme hover:underline' href="mailto:build@measured.ca">build@measured.ca</a></span></p>
-                    </div>
-                    <div>
-                        <h1 className="text-theme font-bold">Careers</h1>
-                        <p>Plenora Architects is always on the lookout for dedicated and enthusiastic architects and intern architects to join our team. Please send your CV, Project Links, and necessary details including portfolio to <span> <a className='hover:text-theme hover:underline' href="mailto:jobs@measured.ca">jobs@measured.ca</a></span></p>
-                    </div>
-                </div>
-            </div>
-            {/* TEAM MEMBERS-------- */}
-            <Title heading={'Meet Our Team'} subHeading={'Creative Minds Shaping Together'} ></Title>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6'>
-                {TeamMembers.slice(0,16).map(member =>
-                     <div key={member.id} className="relative w-full h-[30vh] md:h-[60vh] bg-no-repeat bg-cover group" style={{
-                        backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${member.image})`
-                    }}
-                    >
-                        <div className="absolute bottom-6 md:bottom-12 left-8 md:left-12 text-white font-bold transition-transform duration-300 group-hover:translate-y-[-70px]">
-                            <h1 className="text-3xl"> {member.name} </h1>
-                            <h2 className="text-lg"> {member.position} </h2>
+                    <div className='col-span-4 md:col-span-1 text-justify text-gray grid grid-cols-1 gap-6 items-center'>
+                        <div>
+                            <h1 className="text-theme font-bold">Plenora Architects Ltd.</h1>
+                            <p> #410-1639 West 2nd Avenue <br />
+                                Vancouver, BC <br />
+                                Canada V6J 1H3</p>
+                            <a href="https://www.google.ca/maps/place/1639+W+2nd+Ave,+Vancouver,+BC+V6J+1H3/@49.2699915,-123.1440888,17z/data=!3m1!4b1!4m5!3m4!1s0x548673c96f4a8f7b:0x89c8b0ca78f7ea82!8m2!3d49.269988!4d-123.1419001">Get Directions</a>
                         </div>
-                            {/* Social icons and Details button (Initially hidden, shown on hover) */}
-                        <div className="absolute bottom-6 md:bottom-12 left-8 md:left-12 flex flex-col opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:translate-y-[0] transform translate-y-[50px]">
-                            {/* Social Icons */}
-                            <div className="flex gap-2 text-white">
-                                <a href="https://www.linkedin.com/in/jannatul-afroz-faria-a61086318/"><FaLinkedinIn /></a>
-                                <a href="https://www.facebook.com/profile.php?id=100000633748736"><FaFacebookF /></a>
-                                <a href="https://www.instagram.com/afroz_faria03/"><FaInstagram /></a>
-                                <a href="https://github.com/JannatulAfrozFaria"><PiGithubLogoFill /></a>
+                        <div>
+                            <h1 className="text-theme font-bold">Follow us</h1>
+                            <a className='hover:text-theme hover:underline' href="https://www.instagram.com/">Instagram</a>
+                        </div>
+                        <div>
+                            <h1 className="text-theme font-bold">Press inquiries</h1>
+                            <p> #410-1639 West 2nd Avenue <span> <a className=' text-gray hover:text-theme hover:underline' href="mailto:build@measured.ca">build@measured.ca</a></span></p>
+                        </div>
+                        <div>
+                            <h1 className="text-theme font-bold">Careers</h1>
+                            <p>Plenora Architects is always on the lookout for dedicated and enthusiastic architects and intern architects to join our team. Please send your CV, Project Links, and necessary details including portfolio to <span> <a className='hover:text-theme hover:underline' href="mailto:jobs@measured.ca">jobs@measured.ca</a></span></p>
+                        </div>
+                    </div>
+                </div>
+                {/* TEAM MEMBERS-------- */}
+                <Title heading={'Meet Our Team'} subHeading={'Creative Minds Shaping Together'} ></Title>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6'>
+                    {TeamMembers.slice(0, 16).map(member =>
+                        <div key={member.id} className="relative w-full h-[30vh] md:h-[60vh] bg-no-repeat bg-cover group" style={{
+                            backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url(${member.image})`
+                        }}
+                        >
+                            <div className="absolute bottom-6 md:bottom-12 left-8 md:left-12 text-white font-bold transition-transform duration-300 group-hover:translate-y-[-70px]">
+                                <h1 className="text-3xl"> {member.name} </h1>
+                                <h2 className="text-lg"> {member.position} </h2>
                             </div>
+                            {/* Social icons and Details button (Initially hidden, shown on hover) */}
+                            <div className="absolute bottom-6 md:bottom-12 left-8 md:left-12 flex flex-col opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:translate-y-[0] transform translate-y-[50px]">
+                                {/* Social Icons */}
+                                <div className="flex gap-2 text-white">
+                                    <a href="https://www.linkedin.com/in/jannatul-afroz-faria-a61086318/"><FaLinkedinIn /></a>
+                                    <a href="https://www.facebook.com/profile.php?id=100000633748736"><FaFacebookF /></a>
+                                    <a href="https://www.instagram.com/afroz_faria03/"><FaInstagram /></a>
+                                    <a href="https://github.com/JannatulAfrozFaria"><PiGithubLogoFill /></a>
+                                </div>
 
-                            {/* View Details Button */}
-                            <button className="mt-4 bg-orange-600 hover:bg-white text-white hover:text-orange-600 rounded-lg btn-sm">
-                                View Details
-                            </button>
+                                {/* View Details Button */}
+                                <button className="mt-4 bg-orange-600 hover:bg-white text-white hover:text-orange-600 rounded-lg btn-sm">
+                                    View Details
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                )}
-            </div>
-            <div className='mt-12 md:mt-24'>
-                <NewsLetter></NewsLetter>
-            </div>
+                    )}
+                </div>
+                <div className='mt-12 md:mt-24'>
+                    <NewsLetter></NewsLetter>
+                </div>
+            </PageWrapper>
         </div>
 
     );
