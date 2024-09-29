c708e7248252d6746ac6c499f7a38910b0e3d552
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-30 00:43:33 +0600

    exist attribute added

--- src/app/PageWrapper.jsx
+++ src/app/PageWrapper.jsx
@@ -6,6 +6,7 @@ export const PageWrapper = ({children})=>(
             <motion.div 
             initial={{opacity:0, y: 15}}
             animate={{opacity: 1, y: 0}}
+            exist={{opacity: 1, y: 0}}
              >
                 {children}
             </motion.div>
