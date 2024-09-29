ea0bca36aef32648ad230f47f1d955dbd20baf8d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-30 03:55:03 +0600

    alt proerty added to Image tag

--- src/components/HomePage/WhyChooseUs.jsx
+++ src/components/HomePage/WhyChooseUs.jsx
@@ -25,42 +25,42 @@ const WhyChooseUs = () => {
             image: "https://i.postimg.cc/MG3TFjTq/c3-removebg-preview.png"
         }
     ];
-    
+
     return (
         <div>
             <Title heading={'Why choose Plenora Architects'} subHeading={'Why choose Us'} ></Title>
-            <AosConfig></AosConfig>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 items-center'>
-                {/* EXPERTISE ------LIST------ */}
-                <div className='grid grid-cols-1 gap-3 md:gap-6 w-full md:w-4/5'>
-                    {Expertise.map(service=>
+            <AosConfig>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 items-center'>
+                    {/* EXPERTISE ------LIST------ */}
+                    <div className='grid grid-cols-1 gap-3 md:gap-6 w-full md:w-4/5'>
+                        {Expertise.map(service =>
                             <div key={service.id} className="grid grid-cols-5">
-                            <div className='col-span-1'>
-                                <Image width={50} height={60} alt='service-info' src={service.image}></Image>
+                                <div className='col-span-1'>
+                                    <Image width={50} height={60} alt='service-info' src={service.image}></Image>
+                                </div>
+                                <div className='col-span-4'>
+                                    <h2 className='text-theme text-md  md:text-xl font-bold'>{service.title}</h2>
+                                    <p className=' text-sm md:text-base text-gray my-2 text-justify'> {service.description} </p>
+                                </div>
                             </div>
-                            <div  className='col-span-4'>
-                                <h2 className='text-theme text-md  md:text-xl font-bold'>{service.title}</h2>
-                                <p className=' text-sm md:text-base text-gray my-2 text-justify'> {service.description} </p>
-                            </div>
-                        </div>
                         )}
-                </div>
-                {/* IMAGE------SECTION------ */}
-                <div className='grid grid-cols-2 gap-3 md:gap-6'>
-                    <div className="relative w-full h-full bg-no-repeat bg-cover bottom-right"
-                    style={{
-                        backgroundImage: `url('https://i.postimg.cc/fLXNf3SH/choose1.jpg')` }}>
+                    </div>
+                    {/* IMAGE------SECTION------ */}
+                    <div className='grid grid-cols-2 gap-3 md:gap-6'>
+                        <div className="relative w-full h-full bg-no-repeat bg-cover bottom-right"
+                            style={{
+                                backgroundImage: `url('https://i.postimg.cc/fLXNf3SH/choose1.jpg')`
+                            }}>
 
-                    </div>
-                    {/* <div className='h-full'>
-                        <Image width={500} height={1200} src={'https://i.postimg.cc/fLXNf3SH/choose1.jpg'} ></Image>
-                    </div> */}
-                    <div className='grid grid-cols-1 gap-3 md:gap-6'>
-                        <Image className='top_left' width={500} height={450} src={'https://i.postimg.cc/NfbwDvLw/choose2.jpg'} ></Image>
-                        <Image className='top_right' width={500} height={450} src={'https://i.postimg.cc/MpTJr3jf/choose3.jpg'} ></Image>
+                        </div>
+                        <div className='grid grid-cols-1 gap-3 md:gap-6'>
+                            <Image className='top_left' width={500} height={450} src={'https://i.postimg.cc/NfbwDvLw/choose2.jpg'} alt='interior' ></Image>
+                            <Image className='top_right' width={500} height={450} src={'https://i.postimg.cc/MpTJr3jf/choose3.jpg'} alt='interior' ></Image>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </AosConfig>
+
         </div>
     );
 };
