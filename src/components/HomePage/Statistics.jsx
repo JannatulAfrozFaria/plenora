c2a24f66adaf2eb0bc3d4f369f7fb7c916ffacba
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-04 01:55:04 +0600

    item number put in ScrollTrigger tag

--- src/components/HomePage/Statistics.jsx
+++ src/components/HomePage/Statistics.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import CountUp from 'react-countup';
 import ScrollTrigger from 'react-scroll-trigger';
 const Statistics = () => {
-    const [counterOn,setCounterOn] =useState(false);
+    const [counterOn, setCounterOn] = useState(false);
     const Statistics = [
         {
             id: 1,
@@ -31,31 +31,33 @@ const Statistics = () => {
             image: "https://i.ibb.co/m5P38Hn/l4-r.png"
         }
     ];
-    
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-12 relative w-full  theme-bg
 
         // h-30vh bg-no-repeat bg-cover
 
-        " 
+        "
         // style={{backgroundImage: `linear-gradient(45deg,rgba(7,25,82,0.7),rgba(0,0,0,0.3)), url('https://i.postimg.cc/fLXNf3SH/choose1.jpg')` }}
         >
-             {Statistics.map((item,index)=>
-                            <div key={item.id} className="grid grid-cols-1 gap-0 md:gap-6 text-center p-4 md:p-12">
-                            <div className='flex justify-center relative'>
-                                <Image className='p-6 rounded_custom  ' width={150} height={160} alt='strategy-info' src={item.image}></Image>
-                            </div>
-                            <div  className='text-white'>
-                                <h2 className=' text-base  md:text-xl font-bold'>{item.title}</h2>
-                                <hr className='border-1 border-white mt-3 w-2/3 md:w-3/4 mx-auto' />
-                                <p className=' text-2xl md:text-6xl font-bold my-2 text-center w-full md:w-5/6 mx-auto'> 
-                                    <CountUp start={0} end={`${item.number}`} duration={5} delay={5} />
-                                    +
+            {Statistics.map((item, index) =>
+                <div key={item.id} className="grid grid-cols-1 gap-0 md:gap-6 text-center p-4 md:p-12">
+                    <div className='flex justify-center relative'>
+                        <Image className='p-6 rounded_custom  ' width={150} height={160} alt='strategy-info' src={item.image}></Image>
+                    </div>
+                    <div className='text-white'>
+                        <h2 className=' text-base  md:text-xl font-bold'>{item.title}</h2>
+                        <hr className='border-1 border-white mt-3 w-2/3 md:w-3/4 mx-auto' />
+                        <p className=' text-2xl md:text-6xl font-bold my-2 text-center w-full md:w-5/6 mx-auto'>
+                            <ScrollTrigger>
+                                <CountUp start={0} end={`${item.number}`} duration={5} delay={5} />
+                                +
                                 {/* {item.number}  */}
-                                </p>
-                            </div>
-                        </div>
-                        )}
+                            </ScrollTrigger>
+                        </p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
