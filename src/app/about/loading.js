ecc0dc1cc9f39338e0f07a81561c254fd2a5ca57
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-05 00:45:31 +0600

    padding and margin set up for loading

--- src/app/about/loading.js
+++ src/app/about/loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const loading = () => {
     return (
-        <div>
+        <div className='py-16 w-5/6 mx-auto'>
             <progress className="progress progress-accent w-56" value="70" max="100"></progress>
         </div>
     );
