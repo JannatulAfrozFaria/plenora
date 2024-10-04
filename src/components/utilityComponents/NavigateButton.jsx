c06295de9b227cb09b0ef5ca2277e5624d10ceb6
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-04 22:46:42 +0600

    text and path variables set for navigateBUtton

--- src/components/utilityComponents/NavigateButton.jsx
+++ src/components/utilityComponents/NavigateButton.jsx
@@ -1,10 +1,13 @@
 "use client";
 import React from 'react';
 
-const NavigateButton = () => {
+const NavigateButton = ({text,path}) => {
+    
     return (
         <div>
-            
+             <button onClick={handleNavigate} className='btn btn-basic p-4 w-1/2 md:w-1/3 flex gap-2'>
+                {text}
+            </button>
         </div>
     );
 };
