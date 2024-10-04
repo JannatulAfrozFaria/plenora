8d1f84bbd238528adcb86291871486f99832422f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-05 03:32:43 +0600

    navigate button configured

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -24,7 +24,7 @@ const page = ({params}) => {
                         )} </p>
                     </div>
                     <div>
-                        <NavigateButton></NavigateButton>
+                        <NavigateButton configuration={'btn w-1/2 md:w-1/3  p-4 flex gap-2'} path={'/about'} text={'Go Back'}  ></NavigateButton>
                     </div>
                 </div>
                 <div className="col-span-1"></div>
