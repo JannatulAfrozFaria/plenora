e36cfbec6064811698e017ba86b80d23e323483d
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-05 03:05:22 +0600

    Offer found

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -1,7 +1,9 @@
+import { Offers } from '@/lib/Offers';
 import React from 'react';
 
 const page = ({params}) => {
     const {id} = params;
+    const offer = Offers.find(offer=>offer.id === id);
     return (
         <div>
             
