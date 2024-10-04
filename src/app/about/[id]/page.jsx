fde15bfe307c32dd05226b56fb80fbe3a3dd57e0
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-05 03:35:18 +0600

    image 2 captio added

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -3,10 +3,10 @@ import { Offers } from '@/lib/Offers';
 import Image from 'next/image';
 import React from 'react';
 
-const page = ({params}) => {
-    const {id} = params;
-    const offer = Offers.find(offer=>offer.id === id);
-    const {title,category, logo, images, headingsAndDescriptions,hashtags} = offer;
+const page = ({ params }) => {
+    const { id } = params;
+    const offer = Offers.find(offer => offer.id === id);
+    const { title, category, logo, images, headingsAndDescriptions, hashtags } = offer;
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
@@ -16,21 +16,27 @@ const page = ({params}) => {
                     </div>
                     <div className='p-8'>
                         <h1 className='text-theme text-4xl font-bold'>{title} </h1>
-                        <p>{headingsAndDescriptions.slice(0,1).map(item=>
+                        <div>{headingsAndDescriptions.slice(0, 1).map(item =>
                             <div key={item.heading} >
                                 <h1 className='text-gray font-semibold'> {item.heading} </h1>
                                 <h1 className='text-gray '> {item.description} </h1>
                             </div>
-                        )} </p>
+                        )} </div>
                     </div>
                     <div>
                         <NavigateButton configuration={'btn w-1/2 md:w-1/3  p-4 flex gap-2'} path={'/about'} text={'Go Back'}  ></NavigateButton>
                     </div>
                 </div>
                 <div className="col-span-1">
-                <div>
+                    <div>
                         <Image alt='offer-image' src={images.image2} ></Image>
                     </div>
+                    <div>{headingsAndDescriptions.slice(0, 1).map(item =>
+                        <div key={item.heading} >
+                            <h1 className='text-gray font-semibold'> {item.heading} </h1>
+                            <h1 className='text-gray '> {item.description} </h1>
+                        </div>
+                    )} </div>
                 </div>
             </div>
         </div>
