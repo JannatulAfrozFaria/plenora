3af59123cd624b4c8465b513c7bff81c183e35b5
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-05 03:11:46 +0600

    offer destructured

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const page = ({params}) => {
     const {id} = params;
     const offer = Offers.find(offer=>offer.id === id);
+    const {title,category, logo, images, headingsAndDescriptions,hashtags} = offer;
     return (
         <div>
             
