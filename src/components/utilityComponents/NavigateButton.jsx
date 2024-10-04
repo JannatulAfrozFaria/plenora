d6ba5caec30405e65307159bca3c83e7b374ba2f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-04 22:50:00 +0600

    path set dynamically for navigate button

--- src/components/utilityComponents/NavigateButton.jsx
+++ src/components/utilityComponents/NavigateButton.jsx
@@ -1,8 +1,12 @@
 "use client";
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const NavigateButton = ({text,path}) => {
-    
+    const router = useRouter();
+    const handleNavigate = () =>{
+        router.push(`${path}`);
+    };
     return (
         <div>
              <button onClick={handleNavigate} className='btn btn-basic p-4 w-1/2 md:w-1/3 flex gap-2'>
